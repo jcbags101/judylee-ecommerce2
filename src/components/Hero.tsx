4bcb0a7ea1a4b7292079832909a422933aa12a96
jcbags101 <jude@symph.co>
2024-04-01 21:09:06 +0800

    Update Hero design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,42 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 interface IProps {
   setRatio: (value: number) => void;
 }
 
-
-export default function Hero({setRatio} : IProps) {
+export default function Hero({ setRatio }: IProps) {
   return (
-    <div className='w-full h-screen relative'>
-      <Image src={'/intro.jpg'} alt='no Image' fill className="w-full h-full object-fill hidden md:block " onLoadingComplete={({ naturalWidth, naturalHeight }) =>
-          setRatio(naturalWidth / naturalHeight)
-        } />
-      <div className='w-full flex-col md:hidden h-full relative flex items-center px-3 justify-center text-center'>
-        <Image src={'/mob-intro.jpg'} alt='no image'  fill />
-
-        <h1 className='mb-2 text-xl text-white/90 z-10 font-semibold '>Work hard, lift harder - Get fit, feel fitter with our gear!</h1>
-        <Link href={"/#my-Categories"} className='btn btn-ghost border border-orange-600 text-white/90 hover:bg-orange-600  z-40'>Shop Now</Link>
-
-
+    <div className="flex justify-center items-center px-16 py-16 bg-white max-md:px-5">
+      <div className="mt-36 max-w-full w-[1226px] max-md:mt-10">
+        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+          <div className="flex flex-col w-[61%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col self-stretch my-auto text-black max-md:mt-10 max-md:max-w-full">
+              <div className="text-7xl font-light capitalize leading-[54.76px] max-md:max-w-full max-md:text-4xl">
+                Collections
+              </div>
+              <div className="mt-16 text-3xl capitalize leading-[55px] max-md:mt-10 max-md:max-w-full">
+                you can explore the latest collections of clothing and
+                accessories
+                <br />
+                from various barands here.
+              </div>
+              <div className="flex gap-2 justify-between self-start px-7 py-5 mt-20 text-white bg-gray-800 max-md:px-5 max-md:mt-10">
+                <div className="my-auto text-2xl font-black text-center"></div>
+                <div className="text-3xl capitalize">shop now</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-[39%] max-md:ml-0 max-md:w-full">
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/0a76711ffc49912bec7bb3ea2cd89f63c49fe0730ccd369bb3aa0dfdac13b5b8?placeholderIfAbsent=true"
+              className="grow w-full aspect-[0.79] max-md:mt-10 max-md:max-w-full"
+            />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/utils/Firebase.ts
+++ src/utils/Firebase.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getStorage } from 'firebase/storage';
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcdzlZMD01e-x9teKIWJD4Rj8IymOLbiA",
-  authDomain: "socialapp-9b83f.firebaseapp.com",
-  projectId: "socialapp-9b83f",
-  storageBucket: "socialapp-9b83f.appspot.com",
-  messagingSenderId: "635004374736",
-  appId: "1:635004374736:web:b02daf40b9b19856f9d8d5",
-  measurementId: "G-PS5QJC1GDC"
+  apiKey: "AIzaSyDytVRGsaaY4jtXi6B5gKPgjpvw-7Rwz0k",
+  authDomain: "judylee-ecommerce.firebaseapp.com",
+  projectId: "judylee-ecommerce",
+  storageBucket: "judylee-ecommerce.appspot.com",
+  messagingSenderId: "692437124123",
+  appId: "1:692437124123:web:01dc0cf89ddbc1d7ad64be",
 };
 
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app , "gs://socialapp-9b83f.appspot.com");
+export const storage = getStorage(app, "gs://judylee-ecommerce.appspot.com");
